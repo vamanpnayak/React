7e8afb6d9fc6267ca29216e6f1dd5bd8ac4cae35
vnaya <vnaya@nets.eu>
2018-10-28 23:23:34 +0100

    Added sass to sln (yarn add node-sass)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyHeader from './components/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MyHeader/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React , {Component} from 'react';
+
+class MyHeader extends Component {
+  render() {
+    return (
+      <div> <h1>Developer's webshop</h1> </div>
+    );
+  }
+}
+
+export default MyHeader;
+
